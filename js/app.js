c05639aa1f8e77b65648863db42005030d95e86b
Gabriel <espectrepro@gmail.com>
2023-02-19 10:35:17 -0600

    eliminarHtmlPrevio, messageError, deleteMessageError

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 function iniciarApp(){//Al inciar la app quiero traerme las categorías para el select
 
-    
     const selectCategorias = document.querySelector('#categorias');
     selectCategorias.addEventListener('change', seleccionarCategorias); 
     /*La mayoría de veces el evento para un select es un change, después de ese se leen sus valores.*/
@@ -36,43 +35,67 @@ function iniciarApp(){//Al inciar la app quiero traerme las categorías para el 
     }
     function seleccionarCategorias(e){/*Se le pone e para recuperar el evento, y de esta manera yo puedo
     recuperar el valor seleccionado por el usuario... */
+
         console.log(e.target.value);
         const categorias = e.target.value; 
         /*Y lo que hacemos es mandar llamar los datos que seleccionamos básicamnete, para ello vamos a necesitar
         el segundo amm json por así decirlo....*/
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorias}`;
         //De esta manera las categorías serán dinámicas, se irá trayendo los platillos en base a esa categoría
-        /*Aquí aparece c= y beef, pero lo que nosotros queremos de esto es cambiarle el valor después del igual, 
-        ya que esto sería igual al valor de nuestro platillo, y le pondríamos el valor de la categoría en todo 
-        caso*/
         console.log(url);
-        /*Vemos que se asignó correctamente, ahora lo que tendríamos que hacer es, en base a la categoría,
-        buscar la información de los platillos que podríamos usar para amm mantener esos platillos*/
         fetch(url)
             .then((respuesta) => respuesta.json())
-            .then((resultado) => mostrarPlatillos(resultado.meals))/*para poder acceder automáticamente al valor
-    que engloba los objetos que es (meal) le ponemos la sintaxis de punto(.) y el nombre que nos arroja esa
-    macrocategoría que es meal*/
-
-
-            /*No tengo idea de porque, pero enserio, esto ya está filtrando lo que nosotros tendríamos que 
-    filtrar..., o sea, si colocamos beef, nos va a traer las 42 recetas de beef, si colocamos chicken, nos va
-    a traer las 35 recetas que hay de beef..., sólo tendría que pasar el argumento que guarda los datos de resultado
-    a una función que tome un valor como parámetro y hacer destructuring sobre ese resultado para que me los 
-    muestre....*/
+            .then((resultado) => mostrarPlatillos(resultado.meals))
     };
     function mostrarPlatillos(datosResultadoRecetas = []){/*Ahora que queremos iterar sobre ellos para mostrar los 
-    resultados lo que tenemos que hacer es igualar los datos de los objetos a un arreglo...*/
+        resultados lo que tenemos que hacer es igualar los datos de los objetos a un arreglo...*/
+        eliminarHmtlPrevio();  
+        const headingAlert = document.createElement('h4');
+        headingAlert.classList.add('text-center', 'text-block', 'my-5');
+        /*Aquí hay un problema, si hubo resultados o no hubo 
+        resutados, cómo le podría ha cer? mmmm si sería aquí? jajaja
+        no sería en un catch? ahh claro, si hay datosResultadoRecetas
+        imprmimo algo y si no lo hay no imprimo nada básicamente...*/    
+        
+        if(datosResultadoRecetas.length > 0){
+            headingAlert.textContent = `Su resultado es: `;
+            setTimeout(() => {
+                headingAlert.remove()
+            }, 3000);
+        }else {
+            headingAlert.textContent = `No hay recetas disponibles...`;
+            setTimeout(() => {
+                headingAlert.remove();      
+            }, 3000);
+        };
+        resultado.appendChild(headingAlert);
+        /*Básicamente aquí lo que hicimos fue decir...
+        
+        Si datosResultadoRecetas.lenght(si el largo de nuestro arreglo es verdadero, mayor a 0 o 
+            mejor dicho, si hay algo allí en ese largo de arreglo) entonces coloca un mensaje
+            de resultado: que sería el resultado es este.
+
+            : de lo contrario, si no es mayor a 0, coloca "no hay resultados".
+
+        ahora, lo que tendría que hacer sería eliminar el mensaje después de un tiempo...
+        pero sería sólo el no, 
+
+        este fallo  ocurriría cuando no se encuentren dos seguidos básicamente, podríamos
+        ponerle, si ya existe pues elimínlo, si no existe pss déjalo allí, pero para eso 
+        tendríamos que SEPARAR LAS VARIABLES, EN LO QUE SERÍA EL SI HAY, Y EL SI NO HAY...
+            headingAlert.textContent = datosResultadoRecetas.length ? 'Su resultado: ', : 'No hay resultados disponibles. 
+            esta sería la lógica más corta...
+        */    
+    
+
         console.log(datosResultadoRecetas);
         //Ahora a iterar sobre datosResultadoRecetas
         datosResultadoRecetas.forEach((e) => {
             /*Ahora yo quiero extraer la imagen y el nombre*/
-          const dondePoner = document.querySelector('#resultado'); 
+            const resultado = document.querySelector('#resultado'); 
             const {strMeal, strMealThumb,idMeal} = e; 
             console.log(strMeal, strMealThumb, idMeal);
-            /*Ahora ya teniendo esto lo que quiero hacer es crear un elemento por cada valor y colocarlos en el 
-            html bajo el select de categorias*/
-
+             console.log(e);
             const contenedorReceta = document.createElement('DIV');
             contenedorReceta.classList.add('col-md-4');
 
@@ -87,18 +110,48 @@ function iniciarApp(){//Al inciar la app quiero traerme las categorías para el 
             const recetaCarBody = document.createElement('DIV');
             recetaCarBody.classList.add('card-body');
 
-            console.log(recetaImagen);
+            const recetaHeading = document.createElement('h3');
+            recetaHeading.classList.add('card-title', 'mb-3');
+            recetaHeading.textContent = `${strMeal}`;
+
+            const recetaButton = document.createElement('button');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver receta'; 
+
+            recetaCarBody.append(recetaHeading, recetaButton);
+
             recetaCard.append(recetaImagen, recetaCarBody);
             contenedorReceta.appendChild(recetaCard);
-            dondePoner.appendChild(contenedorReceta);
+            resultado.appendChild(contenedorReceta);
+            /*Los datos a evaluar son, str meal y el id, que es básicamente el nombre de la receta
+            y el id.
+            Para el nombre ya teenmos un lado donde colocarlo, sólo tenemso que crear el elemento
+            */
+
         })
     }
-    /*Ahora, lo que estoy tratando de mostrar es amm los datos, pero no sé cómo hacer que en base a la categoría
-    me muestre los datos que quiero iterando en los objetos, quizás debamos amm convertirlo a un arreglo o algo
-    medianamente parecido.*/
-/**/
-}
+    
+}/*Creo que aquí las Apis podrían empezar a interactuar entre si, de esta manera podríamos obtener
+por ejemplo la información de los platillos de otra api al hacer la consulta.
 
+De igual manera cómo lo hemos estado haciendo, buscando que api tiene esa ifnroamcion, accediendo 
+a esa informacion, englobandola o en un objeto o en un arreglo y despues imprimiéndolo en pantalla.
+*/
+/*Ahora, ocurre un pequeño error...
+
+Esto pasa cuando selecciono una categoría, me manda las recetas y eso.
+
+Pero si vuelvo a seleccionar otra categoría, lo que sucede es que me pone todo lo de la categoría anterior,
+esto lo mantiene y me pone lo de la siguiente categoría hasta abajo.
+
+Bien, yo quiero borrar mi resultado anterior...
+
+*/
+function eliminarHmtlPrevio(){
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild)
+    }
+}
 document.addEventListener('DOMContentLoaded', iniciarApp);
 /**/
 
